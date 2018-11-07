8ba3e59d8c5b2054519a1e7a43920eb8043def00
rafaljank <rafal.jank@powel.com>
2018-11-07 15:30:34 +0100

    Create object_size.sql

---
+++ object_size.sql
@@ -0,0 +1,7 @@
+USE AdventureWorks2012;  
+GO  
+SELECT SUM(used_page_count) AS total_number_of_used_pages,   
+    SUM (row_count) AS total_number_of_rows   
+FROM sys.dm_db_partition_stats  
+WHERE object_id=OBJECT_ID('HumanResources.Employee')    AND (index_id=0 or index_id=1);  
+GO  
